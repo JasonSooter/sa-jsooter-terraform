0c980c916d7c7ee22d981a4ae9dd126f10e7e321
Jason Sooter <7215306+JasonSooter@users.noreply.github.com>
2024-11-19 18:03:15 -0700

    chore(deps): upgrade segment provider to 1.2.0

--- main.tf
+++ main.tf
@@ -2,21 +2,9 @@ terraform {
   required_providers {
     segment = {
       source  = "segmentio/segment"
-      version = "0.10.0"
+      version = "1.2.0"
     }
   }
 }
 
 provider "segment" {}
-
-
-# /**
-#  * Connect an Existing Source
-#  */
-# resource "segment_source" "existing_source" {
-#   slug     = "http_api"
-#   name     = "HTTP API"
-#   enabled  = true
-#   metadata = { id = "iUM16Md8P2" }
-#   settings = jsonencode({})
-# }
